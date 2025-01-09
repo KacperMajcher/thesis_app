5e173742dbdb2e429b3db3d424dc29aae9bf2aab
KacperMajcher <kacpermajcher13@gmail.com>
2025-01-09 18:35:15 +0100

    Initialize repository

---
+++ lib/app/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello, World!'),
+              Icon(Icons.star),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
