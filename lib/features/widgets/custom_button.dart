89bdf4bdf96cb98017b47d5688f9c0311e474463
KacperMajcher <kacpermajcher13@gmail.com>
2025-01-18 18:06:39 +0100

    feat: Add CustomButton widget for reusable button functionality

---
+++ lib/features/widgets/custom_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+  final double height;
+  final double width;
+  final bool disabled;
+  final Color? textColor;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.width = 300,
+    this.height = 50,
+    this.disabled = false,
+    this.textColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF005F8B),
+          foregroundColor: textColor ?? Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: disabled ? null : onPressed,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 16,
+                color: disabled ? Colors.white : textColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
