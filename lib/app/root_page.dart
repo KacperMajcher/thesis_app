f91db0c8a160f369bfcb06bb5078f646a5630534
KacperMajcher <kacpermajcher13@gmail.com>
2025-01-18 13:23:28 +0100

    feat: Add WelcomePage and integrate it into RootPage for improved user onboarding experience

--- lib/app/root_page.dart
+++ lib/app/root_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:thesis_app/features/auth/presentation/login_page.dart';
 import 'package:thesis_app/features/home/presentation/home_page.dart';
+import 'package:thesis_app/features/welcome/presentation/welcome_page.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
@@ -13,7 +14,15 @@ class RootPage extends StatelessWidget {
       builder: (context, snapshot) {
         final user = snapshot.data;
         if (user == null) {
-          return  LoginPage();
+          return WelcomePage(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                ),
+              );
+            },
+          );
         }
         return HomePage(user: user);
       },
