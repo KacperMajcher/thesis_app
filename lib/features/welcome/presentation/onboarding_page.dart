6a54474b686d161211495a90aefa8dd586c3c9a8
KacperMajcher <kacpermajcher13@gmail.com>
2025-01-18 15:00:57 +0100

    feat: Enhance OnboardingPage with dynamic content and improved navigation

--- lib/features/welcome/presentation/onboarding_page.dart
+++ lib/features/welcome/presentation/onboarding_page.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:thesis_app/app/themed_scaffold.dart';
 import 'package:thesis_app/features/auth/presentation/login_page.dart';
+import 'package:thesis_app/features/welcome/domain/models/onboarding.dart';
 import 'package:thesis_app/features/widgets/thesis_year.dart';
 
 class OnboardingPage extends StatefulWidget {
@@ -17,24 +18,91 @@ class _OnboardingPageState extends State<OnboardingPage> {
   int _currentPage = 0;
 
   void _onNextPressed(BuildContext context) {
-    if (_currentPage < 2) {
+    if (_currentPage < _pages.length - 1) {
       _pageController.nextPage(
         duration: const Duration(milliseconds: 300),
         curve: Curves.easeInOut,
       );
     } else {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const LoginPage()),
+        MaterialPageRoute(
+          builder: (context) => const LoginPage(),
+        ),
       );
     }
   }
+
+  static final TextStyle boldStyle = GoogleFonts.inter(
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+    color: const Color(0xFF605D67),
+  );
+
+  final List<Onboarding> _pages = [
+    Onboarding(
+      title: 'Welcome!',
+      description:
+          'This application was created as part of my thesis at Andrzej Frycz Modrzewski University in Krakow during the academic year 2024/2025.',
+    ),
+    Onboarding(
+      title: 'Goal',
+      description:
+          'The goal of the application is to demonstrate the practical use of unit tests through an example of the login functionality in a mobile application.',
+    ),
+    Onboarding(
+      title: 'Who am I?',
+      descriptionSpans: [
+        const TextSpan(text: 'My name is '),
+        TextSpan(
+          text: 'Kacper Majcher',
+          style: boldStyle,
+        ),
+        const TextSpan(
+          text:
+              ', and I am a computer science student at Andrzej Frycz Modrzewski University.',
+        ),
+      ],
+    ),
+    Onboarding(
+      title: 'Who am I?',
+      descriptionSpans: [
+        const TextSpan(text: 'I am passionate about '),
+        TextSpan(
+          text: 'developing mobile applications',
+          style: boldStyle,
+        ),
+        const TextSpan(text: ' and creating efficient, '),
+        TextSpan(
+          text: 'well-tested systems.',
+          style: boldStyle,
+        ),
+        const TextSpan(
+          text:
+              ' As part of this project, I decided to combine these interests by designing and implementing the application following the best practices in software engineering.',
+        ),
+      ],
+    ),
+    Onboarding(
+      title: 'The application',
+      descriptionSpans: [
+        const TextSpan(
+          text: '- An example of the login process using ',
+        ),
+        TextSpan(
+          text: 'Firebase',
+          style: boldStyle,
+        ),
+        const TextSpan(text: '.'),
+      ],
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return ThemedScaffold(
       body: Column(
         children: [
-          SizedBox(height: 60),
+          const SizedBox(height: 60),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Row(
@@ -55,32 +123,27 @@ class _OnboardingPageState extends State<OnboardingPage> {
                     ),
                   ),
                 ),
-                SizedBox(width: 60),
+                const SizedBox(width: 60),
               ],
             ),
           ),
           Expanded(
-            child: PageView(
+            child: PageView.builder(
               controller: _pageController,
+              itemCount: _pages.length,
               onPageChanged: (index) {
                 setState(() {
                   _currentPage = index;
                 });
               },
-              children: [
-                _buildPage(
-                  title: 'Welcome to the App',
-                  description: 'This is the first onboarding screen.',
-                ),
-                _buildPage(
-                  title: 'Track Your Progress',
-                  description: 'Learn how to manage your thesis efficiently.',
-                ),
-                _buildPage(
-                  title: 'Get Started',
-                  description: 'Begin your journey now!',
-                ),
-              ],
+              itemBuilder: (context, index) {
+                final item = _pages[index];
+                return _buildPage(
+                  title: item.title,
+                  description: item.description,
+                  descriptionSpans: item.descriptionSpans,
+                );
+              },
             ),
           ),
           ThesisYear(
@@ -92,7 +155,7 @@ class _OnboardingPageState extends State<OnboardingPage> {
               children: [
                 SmoothPageIndicator(
                   controller: _pageController,
-                  count: 3,
+                  count: _pages.length,
                   effect: const ExpandingDotsEffect(
                     dotHeight: 10,
                     dotWidth: 10,
@@ -111,10 +174,11 @@ class _OnboardingPageState extends State<OnboardingPage> {
 
   Widget _buildPage({
     required String title,
-    required String description,
+    String? description,
+    List<InlineSpan>? descriptionSpans,
   }) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 40),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -126,15 +190,29 @@ class _OnboardingPageState extends State<OnboardingPage> {
             ),
             textAlign: TextAlign.center,
           ),
-          const SizedBox(height: 16),
-          Text(
-            description,
-            style: GoogleFonts.inter(
-              fontWeight: FontWeight.w400,
-              fontSize: 20,
+          const SizedBox(height: 40),
+          if (descriptionSpans != null)
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                style: GoogleFonts.inter(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                  color: const Color(0xFF605D67),
+                ),
+                children: descriptionSpans,
+              ),
+            )
+          else if (description != null)
+            Text(
+              description,
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.w400,
+                color: const Color(0xFF605D67),
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
         ],
       ),
     );
